50d2d5895ba88f4e2ff555db7a1912c2a782c8c6
Matheus Gomes <matheusgomes192@hotmail.com>
2023-05-11 17:26:01 -0300

    Criando estrutura da tela de fazer os exercícios (CurrentWorkout)

--- src/screens/CurrentWorkout/index.js
+++ src/screens/CurrentWorkout/index.js
@@ -1,13 +1,90 @@
 import React from 'react';
 
+import Feather from 'react-native-vector-icons/Feather';
+
+import { useWindowDimensions } from 'react-native';
+
+import { primary } from '../../globals';
+
 import {
     Container,
+
+    Header,
+    ProgressLine,
+    WorkoutInfo,
+    WorkoutInfoNumber,
+    WorkoutInfoText,
+    WorkoutFinish,
+    WorkoutFinishText,
+
+    Content,
+    ContentOver,
+    ContentInfo,
+    ContentInfoLeft,
+    ContentInfoText,
+    ContentInfoLine,
+    ContentInfoRight,
+    ContentInfoNumber,
+    ContentInfoSmallText,
+
+    NextWorkout,
+    NextText,
+    NextName,
+    NextMiniature,
+
+    WorkoutButton,
+    WorkoutButtonText
 } from './styles';
 
 export default function CurrentWorkout () {
+    const window = useWindowDimensions();
+
+    function handleProgressLine () {
+        // Calcular a Largura da linha baseada em quantas séries foram passadas e quantas restam
+    }
+
     return(
         <Container>
-            
+            <Header>
+                <ProgressLine w={window.width / 5 +'px'} />
+
+                <WorkoutInfo>
+                    <WorkoutInfoNumber>5</WorkoutInfoNumber>
+                    <WorkoutInfoText>Séries restantes</WorkoutInfoText>
+                </WorkoutInfo>
+
+                <WorkoutFinish>
+                    <Feather name="flag" color={primary} size={18} />
+                    <WorkoutFinishText>Finalizar treino</WorkoutFinishText>
+                </WorkoutFinish>
+            </Header>
+
+            <Content>
+                <ContentOver>
+                    <ContentInfo>
+                        <ContentInfoLeft>
+                            <ContentInfoText>Rosca direta</ContentInfoText>
+                        </ContentInfoLeft>
+
+                        <ContentInfoLine />
+
+                        <ContentInfoRight>
+                            <ContentInfoNumber>15</ContentInfoNumber>
+                            <ContentInfoSmallText>Vezes</ContentInfoSmallText>
+                        </ContentInfoRight>
+                    </ContentInfo>
+
+                    <NextWorkout>
+                        <NextText>Próximo</NextText>
+                        <NextName>Rosca concentrada</NextName>
+                        <NextMiniature></NextMiniature>
+                    </NextWorkout>
+
+                    <WorkoutButton>
+                        <WorkoutButtonText>Completado</WorkoutButtonText>
+                    </WorkoutButton>
+                </ContentOver>
+            </Content>
         </Container>
     )
 }
--- src/screens/CurrentWorkout/styles.js
+++ src/screens/CurrentWorkout/styles.js
@@ -1,6 +1,131 @@
 import styled from 'styled-components/native';
+import { bold, grayFont, grayish, primary, primaryTransparent, semibold, lightgray, regular } from '../../globals';
+import { secondary } from '../../globals';
 
 export const Container = styled.SafeAreaView`
     background-color: #fff;
     flex: 1;
+`;
+
+export const Header = styled.View`
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    height: 120px;
+    padding: 0 25px;
+`;
+export const ProgressLine = styled.View`
+    background-color: ${secondary};
+    width: ${props=>props.w || 0};
+    height: 3px;
+    border-radius: 3px;
+    position: absolute;
+    top: 0;
+`;
+export const WorkoutInfo = styled.View`
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+`;
+export const WorkoutInfoNumber = styled.Text`
+    color: ${secondary};
+    font-family: ${bold};
+    font-size: 50px;
+`;
+export const WorkoutInfoText = styled.Text`
+    color: ${grayFont};
+    font-family: ${semibold};
+    width: 70px;
+    margin-left: 10px;
+`;
+export const WorkoutFinish = styled.View`
+    background-color: ${lightgray};
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    width: 160px;
+    height: 40px;
+    border-radius: 8px;
+`;
+export const WorkoutFinishText = styled.Text`
+    color: ${secondary};
+    font-family: ${bold};
+    margin-left: 15px;
+`;
+
+export const Content = styled.View`
+    flex: 1;
+    background-color: ${primaryTransparent};
+`;
+export const ContentOver = styled.View`
+    flex: 1;
+    padding: 20px 25px;
+    justify-content: center;
+`;
+export const ContentInfo = styled.View`
+    flex-direction: row;
+    align-items: center;
+    margin-top: 50px;
+`;
+export const ContentInfoLeft = styled.View`
+`;
+export const ContentInfoText = styled.Text`
+    color: ${secondary};
+    font-family: ${bold};
+    font-size: 40px;
+    width: 150px;
+    line-height: 45px;
+`;
+export const ContentInfoLine = styled.View`
+    background-color: ${secondary};
+    width: 1px;
+    height: 100px;
+    margin: 0 45px 0 auto;
+`;
+export const ContentInfoRight = styled.View`
+    align-items: center;
+`;
+export const ContentInfoNumber = styled.Text`
+    color: ${secondary};
+    font-family: ${bold};
+    font-size: 40px;
+`;
+export const ContentInfoSmallText = styled.Text`
+    color: ${secondary};
+    font-family: ${regular};
+`;
+
+export const NextWorkout = styled.View`
+    margin-top: 80px;
+`;
+export const NextText = styled.Text`
+    color: #888;
+    font-family: ${bold};
+`;
+export const NextName = styled.Text`
+    font-family: ${bold};
+    color: ${secondary};
+`;
+export const NextMiniature = styled.View`
+    background-color: #aaa;
+    width: 50px;
+    height: 50px;
+    border-radius: 10px;
+    margin-top: 10px;
+`;
+
+export const WorkoutButton = styled.View`
+    background-color: #fff;
+    align-items: center;
+    justify-content: center;
+    align-self: center;
+    width: 100%;
+    height: 60px;
+    border-radius: 10px;
+    margin-top: auto;
+`;
+export const WorkoutButtonText = styled.Text`
+    color: ${secondary};
+    font-family: ${bold};
+    font-size: 16px;
 `;
