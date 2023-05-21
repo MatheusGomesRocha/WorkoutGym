1a42dbdfd2b21fd8fa21d7a933a98cba2b746e1a
Matheus Gomes <matheusgomes192@hotmail.com>
2023-05-21 13:57:34 -0300

    Criando design da parte inicial da tela de login

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,13 +1,44 @@
 import React from "react";
 
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+
+import { StatusBar } from "react-native";
+
+import { secondary } from "../../globals";
+
 import {
     Container,
+
+    TopColored,
+    LogoArea,
+
+    Content,
+    Title,
+    Text,
+
+    GetStartedButton,
+    GetStartedText
 } from './styles';
 
 export default () => {
     return(
         <Container>
-            
+            <StatusBar backgroundColor={secondary} barStyle={'light-content'} />
+
+            <TopColored>
+                <LogoArea></LogoArea>
+            </TopColored>
+
+            <Content>
+                <Title>WorkoutGym</Title>
+
+                <Text>Plataforma para construir um novo jeito de se exercitar</Text>
+
+                <GetStartedButton>
+                    <GetStartedText>Comece agora grátis</GetStartedText>
+                    <FontAwesome5 style={{marginTop: 2}} name="angle-right" color="#fff" size={14} /> 
+                </GetStartedButton>
+            </Content>
         </Container>
     )
 }
--- src/screens/Login/styles.js
+++ src/screens/Login/styles.js
@@ -1,3 +1,58 @@
 import styled from 'styled-components/native';
+import { bold, grayFont, lightgray, primary, secondary, semibold } from '../../globals';
 
-export const Container = styled.SafeAreaView``;
+export const Container = styled.SafeAreaView`
+    flex: 1;
+    background-color: #fff;
+`;
+
+export const TopColored = styled.View`
+    background-color: ${secondary};
+    height: 300px;
+    border-bottom-left-radius: 120px;
+    border-bottom-right-radius: 120px;
+`;
+export const LogoArea = styled.View`
+    background-color: ${lightgray};
+    align-self: center;
+    bottom: -250px;
+    width: 100px;
+    height: 100px;
+    border-radius: 50px;
+`;
+
+export const Content = styled.View`
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+    padding: 40px 20px;
+`;
+export const Title = styled.Text`
+    color: ${secondary};
+    font-family: ${bold};
+    font-size: 40px;
+    margin-top: 30px;
+`;
+export const Text = styled.Text`
+    color: ${secondary};
+    font-family: ${semibold};
+    margin-top: 40px;
+    font-size: 16px;
+    text-align: center;
+`;
+
+export const GetStartedButton = styled.TouchableOpacity`
+    background-color: ${primary};
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    margin-top: auto;
+    padding: 0 20px;
+    height: 45px;
+    border-radius: 20px;
+`;
+export const GetStartedText = styled.Text`
+    color: #fff;
+    font-family: ${bold};
+    margin-right: 15px;
+`;
