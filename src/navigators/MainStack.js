131eaee8df5cb71d85aefa066f04a167ba24562a
Matheus Gomes <matheusgomes192@hotmail.com>
2023-05-21 13:37:07 -0300

    Criando Tela de Login

--- src/navigators/MainStack.js
+++ src/navigators/MainStack.js
@@ -4,15 +4,17 @@ import { createStackNavigator } from '@react-navigation/stack';
 import AppTab from './AppTab';
 import Home from '../screens/Home';
 import CurrentWorkout from '../screens/CurrentWorkout';
+import Login from '../screens/Login';
 
 const MainStack = createStackNavigator();
 
 export default () => {
     return(
-        <MainStack.Navigator initialRouteName="preload">
+        <MainStack.Navigator initialRouteName="login">
             <MainStack.Screen name="app__tab" component={AppTab} options={{title: null, headerTransparent: true, headerLeft: null}} />
             {/* <MainStack.Screen name="home" component={Home} options={{title: null, headerTransparent: true, headerLeft: null}} /> */}
             <MainStack.Screen name="workoutcurrent" component={CurrentWorkout} options={{headerShown: false}} />
+            <MainStack.Screen name="login" component={Login} options={{headerShown: false}} />
         </MainStack.Navigator>
     )
 }
