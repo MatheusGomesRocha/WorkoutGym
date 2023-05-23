59d15e5fce6066fa8aa78922081029fdb38b9312
Matheus Gomes <matheusgomes192@hotmail.com>
2023-05-23 13:31:53 -0300

    Adicionando Logo da Apple e do Google + Mostrando e escondendo senha

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import Feather from 'react-native-vector-icons/Feather';
 
 import { StatusBar } from "react-native";
+import Animated, { useAnimatedStyle, useSharedValue, withTiming} from "react-native-reanimated";
 
-import { secondary } from "../../globals";
+import { grayish, lightgray, secondary } from "../../globals";
+import Google from '../../assets/images/google.png';
 
 import {
     Container,
@@ -17,28 +20,192 @@ import {
     Text,
 
     GetStartedButton,
-    GetStartedText
+    GetStartedText,
+
+    LoginContent,
+    LogoRow,
+    LogoName,
+    SmallText,
+    Form,
+    InputArea,
+    Label,
+    InputPassword,
+    Input,
+    ShowPasswordButton,
+    Submit,
+    SubmitText,
+    LineRow,
+    Line,
+    LineText,
+    SubmitOptions,
+    SubmitOptionsLogo,
+    SubmitOptionsText,
 } from './styles';
 
 export default () => {
+    const [showPassword, setShowPassword] = useState(true);
+
+    const topColoredHeight = useSharedValue(300);
+    const contentStartOffsetY = useSharedValue(0);
+    const logoOffsetX = useSharedValue(0);
+    const loginOpacity = useSharedValue(0);
+
+    const LoginStyle = useAnimatedStyle(() => {
+        return {
+            flex: 1,
+            position: 'absolute',
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+            opacity: loginOpacity.value,
+        }
+    })
+
+    const ColoredAndLogoArea = () => {
+        const topColoredStyle = useAnimatedStyle(() => {
+            return{
+                height: topColoredHeight.value,
+                backgroundColor: secondary,
+                borderBottomLeftRadius: 120,
+                borderBottomRightRadius: 120,
+            }
+        })
+    
+        const logoOffsetStyle = useAnimatedStyle(() => {
+            return {
+                backgroundColor: lightgray,
+                alignSelf: 'center',
+                alignItems: 'center',
+                justifyContent: 'center',
+                bottom: -250,
+                width: 100,
+                height: 100,
+                borderRadius: 50,
+                transform: [
+                    {translateX: logoOffsetX.value},
+                ],
+                
+            }
+        })
+
+        return (
+            <Animated.View style={topColoredStyle}>
+                <Animated.View style={logoOffsetStyle}>
+                    <LogoArea></LogoArea>
+                </Animated.View>
+            </Animated.View>
+        )
+    }
+
+    const StartContent = () => {
+        const contentStartStyle = useAnimatedStyle(() => {
+            return {
+                flex: 1,
+                zIndex: 9999,
+                transform: [
+                    {translateY: contentStartOffsetY.value}
+                ],
+            }
+        })
+
+        return(
+            <Animated.View style={contentStartStyle}>
+                <Content>
+                    <Title>WorkoutGym</Title>
+
+                    <Text>Plataforma para construir um novo jeito de se exercitar</Text>
+
+                    <GetStartedButton onPress={goToLogin}>
+                        <GetStartedText>Comece agora grátis</GetStartedText>
+                        <FontAwesome5 style={{marginTop: 2}} name="angle-right" color="#fff" size={14} /> 
+                    </GetStartedButton>
+                </Content>
+            </Animated.View>
+        )
+    }
+    
+    function goToLogin() {
+        topColoredHeight.value = withTiming(0, {
+            duration: 500
+        })
+
+        contentStartOffsetY.value = withTiming(1000, {
+            duration: 500
+        })
+
+        logoOffsetX.value = withTiming(-1000, {
+            duration: 1000
+        })
+
+        setTimeout(() => {
+            loginOpacity.value = withTiming(1, {
+                duration: 500,
+            })
+        }, 500)
+    }
+
     return(
         <Container>
             <StatusBar backgroundColor={secondary} barStyle={'light-content'} />
 
-            <TopColored>
-                <LogoArea></LogoArea>
-            </TopColored>
+            <ColoredAndLogoArea />
 
-            <Content>
-                <Title>WorkoutGym</Title>
+            <StartContent />
 
-                <Text>Plataforma para construir um novo jeito de se exercitar</Text>
+            <Animated.View style={LoginStyle}>
+                <LoginContent>
+                    <LogoRow>
+                        <LogoName>WorkoutGym</LogoName>
+                    </LogoRow>
 
-                <GetStartedButton>
-                    <GetStartedText>Comece agora grátis</GetStartedText>
-                    <FontAwesome5 style={{marginTop: 2}} name="angle-right" color="#fff" size={14} /> 
-                </GetStartedButton>
-            </Content>
+                    <SmallText>Treino sem limites</SmallText>
+
+                    <Form>
+                        <InputArea>
+                            <Label>Seu email</Label>
+                            <Input keyboardType="email-address" W='100%' bW='1px' placeholderTextColor="#bbb" placeholder="workout@gmail.com" />
+                        </InputArea>
+
+                        <InputArea>
+                            <Label>Escolha uma senha</Label>
+                            
+                            <InputPassword>
+                                <Input secureTextEntry={showPassword} W='95%' bW='0' placeholderTextColor="#bbb" placeholder="min. 8 caracteres" />
+
+                                <ShowPasswordButton onPress={() => setShowPassword(!showPassword)}>
+                                    {!showPassword ?
+                                        <Feather name="eye" color={grayish} size={20} />
+                                        :
+                                        <Feather name="eye-off" color={grayish} size={20} />
+                                    }
+                                </ShowPasswordButton> 
+                            </InputPassword>
+                        </InputArea>
+                        
+                        <Submit>
+                            <SubmitText>Continue</SubmitText>
+                        </Submit>
+                    </Form>
+
+                    
+                    <LineRow>
+                        <Line />
+                        <LineText>ou</LineText>
+                        <Line />
+                    </LineRow>
+
+                    <SubmitOptions>
+                        <SubmitOptionsLogo source={require('../../assets/images/google.png')} />
+                        <SubmitOptionsText>Cadastre-se com Google</SubmitOptionsText>
+                    </SubmitOptions>
+
+                    <SubmitOptions>
+                        <SubmitOptionsLogo source={require('../../assets/images/apple-logo.png')} />
+                        <SubmitOptionsText>Cadastre-se com Apple</SubmitOptionsText>
+                    </SubmitOptions>
+                </LoginContent>
+            </Animated.View>
         </Container>
     )
 }
--- src/screens/Login/styles.js
+++ src/screens/Login/styles.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components/native';
-import { bold, grayFont, lightgray, primary, secondary, semibold } from '../../globals';
+import { bold, grayFont, grayish, lightgray, primary, secondary, semibold } from '../../globals';
 
 export const Container = styled.SafeAreaView`
     flex: 1;
@@ -8,17 +8,11 @@ export const Container = styled.SafeAreaView`
 
 export const TopColored = styled.View`
     background-color: ${secondary};
-    height: 300px;
-    border-bottom-left-radius: 120px;
-    border-bottom-right-radius: 120px;
 `;
 export const LogoArea = styled.View`
-    background-color: ${lightgray};
-    align-self: center;
-    bottom: -250px;
-    width: 100px;
-    height: 100px;
-    border-radius: 50px;
+    background-color: red;
+    width: 50px;
+    height: 50px;
 `;
 
 export const Content = styled.View`
@@ -55,4 +49,111 @@ export const GetStartedText = styled.Text`
     color: #fff;
     font-family: ${bold};
     margin-right: 15px;
+`;
+
+
+/** --------------------------------------------------------------------------- **/
+
+
+export const LoginContent = styled.View`
+    flex: 1;
+    background-color: #fff;
+    padding: 0 20px;
+    justify-content: center;
+`;
+export const LogoRow = styled.View``;
+export const LogoName = styled.Text`
+    color: ${secondary};
+    font-family: ${bold};
+    text-align: center;
+    font-size: 30px;
+`;
+
+export const SmallText = styled.Text`
+    color: ${secondary};
+    font-family: ${semibold};
+    text-align: center;
+    margin-top: 10px;
+`;
+
+export const Form = styled.View`
+    margin-top: 50px;
+`;
+export const InputArea = styled.View`
+    margin-top: 20px;
+`;
+export const Label = styled.Text`
+    color: ${secondary};
+    font-family: ${bold};
+    margin-bottom: 7px;
+`;
+export const InputPassword = styled.View`
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    border: 1px solid ${grayish};
+    height: 42px;
+    border-radius: 21px;
+    padding-right: 10px;
+`;
+export const Input = styled.TextInput`
+    color: ${secondary};
+    font-family: ${bold};
+    border-color: ${grayish};
+    border-width: ${props=>props.bW};
+    height: 42px;
+    width: ${props=>props.W};
+    border-radius: 21px;
+    padding-left: 10px;
+`;
+export const ShowPasswordButton = styled.TouchableOpacity``;
+export const Submit = styled.TouchableOpacity`
+    background-color: ${primary};
+    align-items: center;
+    justify-content: center;
+    height: 42px;
+    border-radius: 21px;
+    margin-top: 30px;
+`;
+export const SubmitText = styled.Text`
+    color: #fff;
+    font-family: ${bold};
+`;
+
+export const LineRow = styled.View`
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    margin-top: 20px;
+`;
+export const Line = styled.View`
+    background-color: ${grayish};
+    width: 40%;
+    height: 1px;
+`;
+export const LineText = styled.Text`
+    color: ${grayFont};
+    font-family: ${bold};
+    font-size: 16px;
+    line-height: 20px;
+`;
+
+export const SubmitOptions = styled.TouchableOpacity`
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    border: 1px solid ${grayish};
+    height: 48px;
+    border-radius: 24px;
+    margin-top: 20px;
+`;
+export const SubmitOptionsLogo = styled.Image`
+    width: 15px;
+    height: 15px;
+`;
+export const SubmitOptionsText = styled.Text`
+    color: ${secondary};
+    font-family: ${bold};
+    margin-left: 13px;
+    font-size: 15px;
 `;
